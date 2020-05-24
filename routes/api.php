b4b284db6f87a2aec7e124cc93032162803a4086
fajaaro <48170341+fajaaro@users.noreply.github.com>
2020-05-24 10:19:14 +0700

    Modify api/user route's response

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json([
+    	'user' => $request->user(),
+    	'status' => 'Success!',
+    ]);
 });
 
 Route::name('api.')->namespace('Api')->group(function() {
